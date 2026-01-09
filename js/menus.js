3e21e2c970ab798f195bee75f2c9320289afa57a
SantiCP13 <santiago.castillo.parra@gmail.com>
2026-01-09 18:44:38 -0500

    prueba

--- js/menus.js
+++ js/menus.js
@@ -26,10 +26,10 @@ function applyLanguage(lang) {
 
     if (currentFlag && currentText) {
         if (lang === 'es') {
-            currentFlag.src = '/splendorherbs/img/co.png';
+            currentFlag.src = '/img/co.png';
             currentText.textContent = 'ESP';
         } else {
-            currentFlag.src = '/splendorherbs/img/us.png';
+            currentFlag.src = '/img/us.png';
             currentText.textContent = 'EN';
         }
     }
